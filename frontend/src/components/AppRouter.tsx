2073dbc4d36811b3fe999421b91c21a7d61f1b16
miracleexotic <mai_nutthawat@hotmail.com>
2023-05-24 21:09:49 +0700

    :hammer: Edit route path - add /:id

--- frontend/src/components/AppRouter.tsx
+++ frontend/src/components/AppRouter.tsx
@@ -5,13 +5,39 @@ import {
 } from "react-router-dom";
 
 import Navbar from './App/Navbar';
-import Home from './App/Home';
+import HomePage from './App/Home';
+import AddPage from './App/Add';
+import EditPage from './App/Edit';
+import ProfilePage from './App/Profile';
+import SettingPage from './App/Setting';
+import ViewPage from './App/View';
+
 
 function AppRouter() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
+      element: <HomePage />,
+    },
+    {
+      path: "/add",
+      element: <AddPage />,
+    },
+    {
+      path: "/edit/:editID",
+      element: <EditPage />,
+    },
+    {
+      path: "/profile",
+      element: <ProfilePage />,
+    },
+    {
+      path: "/setting",
+      element: <SettingPage />,
+    },
+    {
+      path: "/view/:viewID",
+      element: <ViewPage />,
     },
   ]);
 
