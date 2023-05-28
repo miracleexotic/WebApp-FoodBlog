8707cc6832ef403881c8bf329930b8da21022755
miracleexotic <mai_nutthawat@hotmail.com>
2023-05-28 14:56:52 +0700

    close #11 - Create Schema.

---
+++ backend/entity/schema.go
@@ -0,0 +1,98 @@
+package entity
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Email    string `gorm:"uniqueIndex"`
+	Username string
+	Password string `json:"-"`
+	Image    string
+
+	// 1 user เป็นเจ้าของได้หลาย post
+	Posts []Post `gorm:"foreignKey:AuthorID"`
+
+	// 1 user สามารถถูกใจได้หลาย likePost
+	Likes []LikePost `gorm:"foreignKey:UserLikeID"`
+
+	// 1 user เป็นเจ้าของได้หลาย comment
+	Comments []CommentPost `gorm:"foreignKey:UserCommentID"`
+
+	// 1 user สามารถแสดงความรู้สึกได้หลาย emotionalComment
+	EmotionalComments []EmotionalComment `gorm:"foreignKey:UserEmotionalID"`
+}
+
+type Post struct {
+	gorm.Model
+	Title     string
+	Subject   string
+	Image     string
+	Create_at time.Time
+
+	// AuthorID ทำหน้าที่เป็น FK
+	AuthorID *uint
+	Author   User `gorm:"references:id"`
+
+	// 1 post สามารถถูกใจได้หลาย likePost
+	Likes []LikePost `gorm:"foreignKey:PostID"`
+
+	// 1 post สามารถคอมเมนต์ได้หลาย commentPost
+	Comments []CommentPost `gorm:"foreignKey:PostID"`
+}
+
+type LikePost struct {
+	gorm.Model
+
+	// PostID ทำหน้าที่เป็น FK
+	PostID *uint
+	Post   Post `gorm:"references:id"`
+
+	// UserLikeID ทำหน้าที่เป็น FK
+	UserLikeID *uint
+	UserLike   User `gorm:"references:id"`
+}
+
+type CommentPost struct {
+	gorm.Model
+	Comment   string
+	Create_at time.Time
+
+	// PostID ทำหน้าที่เป็น FK
+	PostID *uint
+	Post   Post `gorm:"references:id"`
+
+	// UserCommentID ทำหน้าที่เป็น FK
+	UserCommentID *uint
+	UserComment   User `gorm:"references:id"`
+
+	// 1 commentPost สามารถมีได้หลาย emotionalComment
+	EmotionalComments []EmotionalComment `gorm:"foreignKey:EmotionalCommentID"`
+}
+
+type EmotionalList struct {
+	gorm.Model
+	Name string
+
+	// 1 emotionalList สามารถอยู่ได้ในหลาย emotionalComment
+	EmotionalComments []EmotionalComment `gorm:"foreignKey:EmotionalID"`
+}
+
+type EmotionalComment struct {
+	gorm.Model
+
+	// EmotionalCommentID ทำหน้าที่เป็น FK
+	EmotionalCommentID *uint
+	EmotionalComment   CommentPost `gorm:"references:id"`
+
+	// EmotionalID ทำหน้าที่เป็น FK
+	EmotionalID *uint
+	Emotional   EmotionalList `gorm:"references:id"`
+
+	// UserEmotionalID ทำหน้าที่เป็น FK
+	UserEmotionalID *uint
+	UserEmotional   User `gorm:"references:id"`
+}
