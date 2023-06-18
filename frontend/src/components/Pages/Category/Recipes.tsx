0135e5455294ec70bb53f2576ab702b7b802c507
miracleexotic <mai_nutthawat@hotmail.com>
2023-06-18 18:14:26 +0700

    :hammer: Add Category and filter search.

--- frontend/src/components/Pages/Category/Recipes.tsx
+++ frontend/src/components/Pages/Category/Recipes.tsx
@@ -9,6 +9,27 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import Chip from '@mui/material/Chip';
 import { grey } from '@mui/material/colors';
 import Moment from 'moment';
+import moment from 'moment';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+import SearchIcon from '@mui/icons-material/Search';
+import SettingsIcon from '@mui/icons-material/Settings';
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import Backdrop from '@mui/material/Backdrop';
+import CloseIcon from '@mui/icons-material/Close';
+import Checkbox from '@mui/material/Checkbox';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormHelperText from '@mui/material/FormHelperText';
 
 import { PostInterface } from '../../../models/IPost';
 
@@ -189,6 +210,14 @@ function PostComponent(props: DataPostInterface) {
   )
 }
 
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    width: '100%',   
+  },
+}));
+
 function RecipesPage() {
 
   const PageName = "Recipes";
@@ -234,13 +263,60 @@ function RecipesPage() {
       .then((response) => response.json())
       .then((res) => {
         if (res.data) {
-          setPosts(res.data)
+          setPosts((post) => {
+            setFilteredList(res.data)
+            return res.data
+          })
           // setSuccess(true);
         } else {
           // setError(true);
         }
       });
   }
+
+  const [openQuerySetting, setOpenQuerySetting] = React.useState(false);
+
+  const [expanded, setExpanded] = React.useState<string | false>(false);
+  const handleChange = (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  const [startDate, setStartDate] = React.useState<Date | null | undefined>(null);
+  const [endDate, setEndDate] = React.useState<Date | null | undefined>(null);
+
+  const [query, setQuery] = React.useState({
+    Search: "",
+    From: {
+      Title: true,
+      Subject: true,
+    },
+    Category: {
+      Reviews: true,
+      Recipes: true,
+      Jobs: true,
+      Promote: true,
+      Ask: true,
+
+    }
+  });
+
+  const [filteredList, setFilteredList] = React.useState(posts);
+  function filterBySearch() {
+    var updatedList = [...posts];
+    updatedList = updatedList.filter((item: PostInterface) => {
+      return (
+        ((query.From.Title   && item.Title.toLowerCase().indexOf(query.Search.toLowerCase()) !== -1) ||
+         (query.From.Subject && item.Subject.toLowerCase().indexOf(query.Search.toLowerCase()) !== -1)) &&
+       !((startDate != null  && Date.parse(startDate.toISOString()) > Date.parse(moment(item.Create_at).toISOString())) || 
+         (endDate   != null  && Date.parse(endDate.toISOString()) < Date.parse(moment(item.Create_at).toISOString())))
+      )
+    });
+    setFilteredList(updatedList);
+  };
+
+  React.useEffect(() => {
+    filterBySearch();
+  }, [query, startDate, endDate]);
 
   React.useEffect(() => {
     getCategory()
@@ -250,9 +326,152 @@ function RecipesPage() {
     <Container sx={{ display: 'flex', justifyContent: 'center' }}>
       <Box sx={{ display: 'block', justifyContent: 'center' }}>
 
-          {posts.map((data: PostInterface) => 
-            <PostComponent Post={data}  />
-          )}
+        <Box sx={{ width: '700px', height: '40px', boxShadow: '1px 2px 9px #9999', margin: '2em', padding: '1em', borderRadius: '20px' }}>
+          <Box sx={{ display: 'flex', marginTop: '-0.4em'}}>
+            <SearchIcon sx={{color: 'grey'}}/>
+            <Box sx={{flexGrow: 1, marginTop: '-0.5em', marginLeft: 2}}>
+              <StyledInputBase
+                id="Search"
+                placeholder="Search…"
+                sx={{width: '100%'}}
+                onChange={(e: any) => setQuery({...query, Search: e.target.value})}
+                value={query.Search}
+              />
+            </Box>
+            <IconButton sx={{marginTop: '-0.45em', marginRight: -1}}>
+              <SettingsIcon 
+                sx={{color: 'grey'}}
+                onClick={() => setOpenQuerySetting(!openQuerySetting)}
+              />
+            </IconButton>
+            <Backdrop
+              sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+              open={openQuerySetting}
+            >
+              <Box sx={{ background: 'black', width: '500px', boxShadow: '1px 2px 9px #9999', borderRadius: '20px', padding: 1 }}>
+                <Box sx={{ display: 'flex', background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)', borderRadius: '20px' }} >
+                  <SearchIcon sx={{color: '#fff', marginTop: 1, marginLeft: 1}}/>
+                  <Box sx={{flexGrow: 1, marginTop: 0.2, marginLeft: 2}}>
+                    <StyledInputBase
+                      id="Search"
+                      placeholder="Search…"
+                      sx={{width: '100%'}}
+                      onChange={(e: any) => setQuery({...query, Search: e.target.value})}
+                      value={query.Search}
+                    />
+                  </Box>
+                  <IconButton sx={{ marginTop: -0.25 }}
+                    onClick={() => {
+                      if (query.From.Title || query.From.Subject) {
+                        setOpenQuerySetting(!openQuerySetting); 
+                      }
+                    }}
+                  >
+                    <CloseIcon sx={{color: '#fff'}} />
+                  </IconButton>
+                </Box> 
+                
+                <Box sx={{ marginTop: 1 }}>
+                  <Accordion 
+                    expanded={expanded === 'panel-Advanced-settings'} 
+                    onChange={handleChange('panel-Advanced-settings')}
+                    sx={{ background: 'linear-gradient(90deg, #2CD3E1 30%, #59b1f6 90%)', color: '#fff', borderRadius: '10px', }}
+                  >
+                    <AccordionSummary
+                      expandIcon={<ExpandMoreIcon sx={{ color: '#fff' }}/>}
+                      aria-controls="panel1bh-content"
+                      id="panel1bh-header"
+                    >
+                      <Typography sx={{ width: '50%', flexShrink: 0 }}>
+                        Datetime Filter
+                      </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails
+                      sx={{background: '#fff', borderRadius: '0 0 4px 4px'}}
+                    >
+                      <Box>
+                        <LocalizationProvider dateAdapter={AdapterMoment} >
+                          <DemoContainer components={['DatePicker']} >
+                            <DatePicker 
+                              label="Start date"
+                              format='DD/MM/YYYY'
+                              sx={{ display: 'flex', flexGrow: 1 }}
+                              value={startDate} 
+                              onChange={(newValue) => setStartDate(newValue)}
+                            />
+                            <DatePicker 
+                              label="End date"
+                              format='DD/MM/YYYY'
+                              sx={{ display: 'flex', flexGrow: 1 }}
+                              value={endDate} 
+                              onChange={(newValue) => setEndDate(newValue)}
+                            />
+                          </DemoContainer>
+                        </LocalizationProvider>
+                      </Box>
+                      <Box sx={{ marginTop: 0.5, display: 'flex' }}>
+                        <Button 
+                          size='small'
+                          variant="contained"
+                          sx={{display: 'flex', flexGrow: 1}}
+                          onClick={() => {
+                            setStartDate(null)
+                            setEndDate(null)
+                          }}
+                        >
+                          Clear
+                        </Button>
+                      </Box>
+                    </AccordionDetails>
+                  </Accordion>
+                </Box>
+            
+                <Box sx={{display: 'flex', marginTop: 1}}>
+                  <Box sx={{ marginLeft: 1.5, marginTop: 1, width: '40%', textAlign: 'left' }}>
+                    <Typography>
+                      Search from
+                    </Typography>
+                  </Box>
+                  <Box>
+                    <FormControl component="fieldset"
+                      required
+                      error={!(query.From.Title || query.From.Subject)}
+                    >
+                      <FormGroup aria-label="position" row>
+                        <FormControlLabel
+                          value="Title"
+                          control={<Checkbox 
+                            sx={{color: '#fff'}}
+                            onChange={(e: any) => setQuery({ ...query, From: {...query.From, Title: e.target.checked}})}
+                            checked={query.From.Title}
+                          />}
+                          label="Title"
+                          labelPlacement="end"
+                        />
+                        <FormControlLabel
+                          value="Content"
+                          control={<Checkbox
+                            sx={{color: '#fff'}} 
+                            onChange={(e: any) => setQuery({ ...query, From: {...query.From, Subject: e.target.checked}})}
+                            checked={query.From.Subject}
+                          />}
+                          label="Content"
+                          labelPlacement="end"
+                        />
+                      </FormGroup>
+                      <FormHelperText sx={{color: '#fff'}}>*At least 1 or more filter.</FormHelperText>
+                    </FormControl>
+                  </Box>
+                </Box>
+            
+              </Box>
+            </Backdrop>
+          </Box>
+        </Box>
+
+        {filteredList.map((data: PostInterface) => 
+          <PostComponent Post={data}  />
+        )}
 
       </Box>
     </Container>
--- frontend/src/components/Pages/Home.tsx
+++ frontend/src/components/Pages/Home.tsx
@@ -9,8 +9,30 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import Chip from '@mui/material/Chip';
 import { grey } from '@mui/material/colors';
 import Moment from 'moment';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+import SearchIcon from '@mui/icons-material/Search';
+import SettingsIcon from '@mui/icons-material/Settings';
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import Backdrop from '@mui/material/Backdrop';
+import CloseIcon from '@mui/icons-material/Close';
+import Checkbox from '@mui/material/Checkbox';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormHelperText from '@mui/material/FormHelperText';
+import Pagination from '@mui/material/Pagination';
 
 import { PostInterface } from '../../models/IPost';
+import moment from 'moment';
 
 
 interface DataPostInterface {
@@ -189,6 +211,14 @@ function PostComponent(props: DataPostInterface) {
   )
 }
 
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    width: '100%',   
+  },
+}));
+
 function HomePage() {
 
   const [posts, setPosts] = React.useState<PostInterface[]>([]);
@@ -207,7 +237,13 @@ function HomePage() {
       .then((response) => response.json())
       .then((res) => {
         if (res.data) {
-          setPosts(res.data)
+          setPosts((post) => {
+              setFilteredList(res.data.filter((post: PostInterface, idx: number) => {
+                return 0 <= idx && idx < 10
+              }))
+              setPageCount(Math.floor(res.data.length / 10)+1)
+            return res.data
+          })
           // setSuccess(true);
         } else {
           // setError(true);
@@ -215,17 +251,322 @@ function HomePage() {
       });
   }
 
+  const [openQuerySetting, setOpenQuerySetting] = React.useState(false);
+
+  const [expanded, setExpanded] = React.useState<string | false>(false);
+  const handleChange = (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  const [startDate, setStartDate] = React.useState<Date | null | undefined>(null);
+  const [endDate, setEndDate] = React.useState<Date | null | undefined>(null);
+
+  const [query, setQuery] = React.useState({
+    Search: "",
+    From: {
+      Title: true,
+      Subject: true,
+    },
+    Category: {
+      Reviews: true,
+      Recipes: true,
+      Jobs: true,
+      Promote: true,
+      Ask: true,
+
+    }
+  });
+
+  const [filteredList, setFilteredList] = React.useState(posts);
+  function filterBySearch() {
+    var updatedList = [...posts];
+    updatedList = updatedList.filter((item: PostInterface) => {
+      if (
+          ((query.From.Title   && item.Title.toLowerCase().indexOf(query.Search.toLowerCase()) !== -1) ||
+           (query.From.Subject && item.Subject.toLowerCase().indexOf(query.Search.toLowerCase()) !== -1)) &&
+         !((startDate != null  && Date.parse(startDate.toISOString()) > Date.parse(moment(item.Create_at).toISOString())) || 
+           (endDate   != null  && Date.parse(endDate.toISOString()) < Date.parse(moment(item.Create_at).toISOString())))
+         ) {
+            if ((query.Category.Reviews && item.Category.Name === "Reviews")) {
+              return true
+            } else if ((query.Category.Recipes && item.Category.Name === "Recipes")) {
+              return true
+            } else if ((query.Category.Jobs && item.Category.Name === "Jobs")) {
+              return true
+            } else if ((query.Category.Promote && item.Category.Name === "Promote")) {
+              return true
+            } else if ((query.Category.Ask && item.Category.Name === "Ask")) {
+              return true
+            }
+      }
+      return false
+    });
+    if ((page*10) <= updatedList.length) {
+      setFilteredList(updatedList.filter((post: PostInterface, idx: number) => {
+        return (page*10)-10 <= idx && idx < (page*10)
+      }))
+    } else {
+      setFilteredList(updatedList.filter((post: PostInterface, idx: number) => {
+        return (page*10)-10 <= idx
+      }))
+    }
+    setPageCount(Math.floor(updatedList.length / 10)+1)
+    window.scrollTo(0, 0)
+  };
+
+  const [page, setPage] = React.useState(1);
+  const [pageCount, setPageCount] = React.useState(0);
+  const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
+
   React.useEffect(() => {
-    getPosts()
-  }, [])
+    filterBySearch();
+  }, [query, startDate, endDate, page]);
+
+  React.useEffect(() => {
+    getPosts();
+  }, []);
 
   return (
     <Container sx={{ display: 'flex', justifyContent: 'center' }}>
       <Box sx={{ display: 'block', justifyContent: 'center' }}>
 
-          {posts.map((data: PostInterface) => 
-            <PostComponent Post={data}  />
-          )}
+        <Box sx={{ width: '700px', height: '40px', boxShadow: '1px 2px 9px #9999', margin: '2em', padding: '1em', borderRadius: '20px' }}>
+          <Box sx={{ display: 'flex', marginTop: '-0.4em'}}>
+            <SearchIcon sx={{color: 'grey'}}/>
+            <Box sx={{flexGrow: 1, marginTop: '-0.5em', marginLeft: 2}}>
+              <StyledInputBase
+                id="Search"
+                placeholder="Search…"
+                sx={{width: '100%'}}
+                onChange={(e: any) => setQuery({...query, Search: e.target.value})}
+                value={query.Search}
+              />
+            </Box>
+            <IconButton sx={{marginTop: '-0.45em', marginRight: -1}}>
+              <SettingsIcon 
+                sx={{color: 'grey'}}
+                onClick={() => setOpenQuerySetting(!openQuerySetting)}
+              />
+            </IconButton>
+            <Backdrop
+              sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+              open={openQuerySetting}
+            >
+              <Box sx={{ background: 'black', width: '500px', boxShadow: '1px 2px 9px #9999', borderRadius: '20px', padding: 1 }}>
+                <Box sx={{ display: 'flex', background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)', borderRadius: '20px' }} >
+                  <SearchIcon sx={{color: '#fff', marginTop: 1, marginLeft: 1.5}}/>
+                  <Box sx={{flexGrow: 1, marginTop: 0.2, marginLeft: 2}}>
+                    <StyledInputBase
+                      id="Search"
+                      placeholder="Search…"
+                      sx={{width: '100%'}}
+                      onChange={(e: any) => setQuery({...query, Search: e.target.value})}
+                      value={query.Search}
+                    />
+                  </Box>
+                  <IconButton sx={{ marginTop: -0.25 }}
+                    onClick={() => {
+                      if ((query.From.Title || query.From.Subject) &&
+                          (query.Category.Reviews || query.Category.Recipes || query.Category.Promote || query.Category.Jobs || query.Category.Ask)) {
+                            setOpenQuerySetting(!openQuerySetting); 
+                          }
+                    }}
+                  >
+                    <CloseIcon sx={{color: '#fff'}} />
+                  </IconButton>
+                </Box>
+
+                <Box sx={{ marginTop: 1 }}>
+                  <Accordion 
+                    expanded={expanded === 'panel-Advanced-settings'} 
+                    onChange={handleChange('panel-Advanced-settings')}
+                    sx={{ background: 'linear-gradient(90deg, #2CD3E1 30%, #59b1f6 90%)', color: '#fff', borderRadius: '10px', }}
+                  >
+                    <AccordionSummary
+                      expandIcon={<ExpandMoreIcon sx={{ color: '#fff' }}/>}
+                      aria-controls="panel1bh-content"
+                      id="panel1bh-header"
+                    >
+                      <Typography sx={{ width: '50%', flexShrink: 0 }}>
+                        Datetime Filter
+                      </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails
+                      sx={{background: '#fff', borderRadius: '0 0 4px 4px'}}
+                    >
+                      <Box>
+                        <LocalizationProvider dateAdapter={AdapterMoment} >
+                          <DemoContainer components={['DatePicker']} >
+                            <DatePicker 
+                              label="Start date"
+                              format='DD/MM/YYYY'
+                              sx={{ display: 'flex', flexGrow: 1 }}
+                              value={startDate} 
+                              onChange={(newValue) => setStartDate(newValue)}
+                            />
+                            <DatePicker 
+                              label="End date"
+                              format='DD/MM/YYYY'
+                              sx={{ display: 'flex', flexGrow: 1 }}
+                              value={endDate} 
+                              onChange={(newValue) => setEndDate(newValue)}
+                            />
+                          </DemoContainer>
+                        </LocalizationProvider>
+                      </Box>
+                      <Box sx={{ marginTop: 0.5, display: 'flex' }}>
+                        <Button 
+                          size='small'
+                          variant="contained"
+                          sx={{display: 'flex', flexGrow: 1}}
+                          onClick={() => {
+                            setStartDate(null)
+                            setEndDate(null)
+                          }}
+                        >
+                          Clear
+                        </Button>
+                      </Box>
+                    </AccordionDetails>
+                  </Accordion>
+                </Box>  
+            
+                <Box sx={{display: 'flex', marginTop: 1}}>
+                  <Box sx={{ marginLeft: 1.5, marginTop: 1, width: '40%', textAlign: 'left' }}>
+                    <Typography>
+                      Search from
+                    </Typography>
+                  </Box>
+                  <Box>
+                    <FormControl component="fieldset"
+                      required
+                      error={!(query.From.Title || query.From.Subject)}
+                    >
+                      <FormGroup aria-label="position" row>
+                        <FormControlLabel
+                          value="Title"
+                          control={<Checkbox 
+                            sx={{color: '#fff'}}
+                            onChange={(e: any) => setQuery({ ...query, From: {...query.From, Title: e.target.checked}})}
+                            checked={query.From.Title}
+                          />}
+                          label="Title"
+                          labelPlacement="end"
+                        />
+                        <FormControlLabel
+                          value="Content"
+                          control={<Checkbox
+                            sx={{color: '#fff'}} 
+                            onChange={(e: any) => setQuery({ ...query, From: {...query.From, Subject: e.target.checked}})}
+                            checked={query.From.Subject}
+                          />}
+                          label="Content"
+                          labelPlacement="end"
+                        />
+                      </FormGroup>
+                      <FormHelperText sx={{color: '#fff'}}>*At least 1 or more filter.</FormHelperText>
+                    </FormControl>
+                  </Box>
+                </Box>
+            
+                <Box sx={{display: 'flex', marginTop: 1}}>
+                  <Box sx={{ marginLeft: 1.5, marginTop: 1, width: '40%', textAlign: 'left' }}>
+                    <Typography>
+                      Category
+                    </Typography>
+                  </Box>
+                  <Box>
+                    <FormControl component="fieldset"
+                      required
+                      error={!(query.Category.Reviews || query.Category.Recipes || query.Category.Promote || query.Category.Jobs || query.Category.Ask)}
+                    >
+                      <FormGroup aria-label="position">
+                        <FormControlLabel
+                          value="Reviews"
+                          control={<Checkbox 
+                            sx={{color: '#fff'}}
+                            onChange={(e: any) => setQuery({ ...query, Category: { 
+                              ...query.Category,
+                              Reviews: e.target.checked, 
+                            }})}
+                            checked={query.Category.Reviews}
+                          />}
+                          label="Reviews"
+                          labelPlacement="end"
+                        />
+                        <FormControlLabel
+                          value="Recipes"
+                          control={<Checkbox
+                            sx={{color: '#fff'}} 
+                            onChange={(e: any) => setQuery({ ...query, Category: { 
+                              ...query.Category,
+                              Recipes: e.target.checked, 
+                            }})}
+                            checked={query.Category.Recipes}
+                          />}
+                          label="Recipes"
+                          labelPlacement="end"
+                        />
+                        <FormControlLabel
+                          value="Jobs"
+                          control={<Checkbox
+                            sx={{color: '#fff'}} 
+                            onChange={(e: any) => setQuery({ ...query, Category: { 
+                              ...query.Category,
+                              Jobs: e.target.checked, 
+                            }})}
+                            checked={query.Category.Jobs}
+                          />}
+                          label="Jobs"
+                          labelPlacement="end"
+                        />
+                        <FormControlLabel
+                          value="Promote"
+                          control={<Checkbox
+                            sx={{color: '#fff'}} 
+                            onChange={(e: any) => setQuery({ ...query, Category: { 
+                              ...query.Category,
+                              Promote: e.target.checked, 
+                            }})}
+                            checked={query.Category.Promote}
+                          />}
+                          label="Promote"
+                          labelPlacement="end"
+                        />
+                        <FormControlLabel
+                          value="Ask"
+                          control={<Checkbox
+                            sx={{color: '#fff'}} 
+                            onChange={(e: any) => setQuery({ ...query, Category: { 
+                              ...query.Category,
+                              Ask: e.target.checked 
+                            }})}
+                            checked={query.Category.Ask}
+                          />}
+                          label="Ask"
+                          labelPlacement="end"
+                        />
+                      </FormGroup>
+                      <FormHelperText sx={{color: '#fff'}}>*At least 1 or more filter.</FormHelperText>
+                    </FormControl>
+                  </Box>
+                </Box>
+            
+              </Box>
+            </Backdrop>
+          </Box>
+        </Box>
+        
+        {filteredList.map((data: PostInterface) => 
+          <PostComponent Post={data}  />
+        )}
+
+        {/* <Pagination count={Math.floor(filteredList.length / 10)+1} page={page} onChange={handlePageChange}  */}
+        <Pagination count={pageCount} page={page} onChange={handlePageChange} 
+          sx={{display: 'flex', flexGrow: 1, margin: '2em', padding: '1em', justifyContent: 'center'}}
+        />
 
       </Box>
     </Container>
